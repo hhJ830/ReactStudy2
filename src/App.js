0c46bd9619225067f67288679692425a0d6b5590
장현희 <99874673+hhJ830@users.noreply.github.com>
2024-01-17 22:09:09 +0900

    01.17

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+// eslint-disable-next-line
+
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
-
+import { Button, Navbar, Container, Row, Col, Nav } from 'react-bootstrap';
+import data from './data.js';
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <Navbar bg="light" data-bs-theme="light" >
-        <Container className='justify-content-between'>
-          <div>
-            <Nav>
-              <Navbar.Brand href="#home" className='font'>Dior</Navbar.Brand>
-
-              <Nav.Link href="#home" style={{ marginLeft: '5rem' }}>Home</Nav.Link>
-            </Nav>
-          </div>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Row>
+            <Col>
+              <Nav>
+                <Navbar.Brand href="#home" className='font' style={{ fontSize: 28 }}>NIKE</Navbar.Brand>
+              </Nav>
+            </Col>
+            
+            <Col className="d-flex justify-content-end">
+              <Nav>
+                <Nav.Link href="#home" className='mr-2' style={{ marginTop: '5px' }}>Home</Nav.Link>
+              </Nav>
+            </Col>
+          </Row>
           <div>
             <Nav>
               <Nav.Link href="#cart" className='ml-2'>Cart</Nav.Link>
@@ -25,8 +38,26 @@ function App() {
         </Container>
       </Navbar>
 
-      <Button variant="primary">Primary</Button>{' '}
-
+      <div className="main-bg"></div>
+      <Container>
+        <Row style={{ marginTop: '40px' }}>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
+            <h4>{ shoes[0].title }</h4>
+            <p>{ shoes[0].price }</p>
+          </Col>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"></img>
+            <h4>{ shoes[1].title }</h4>
+            <p>{ shoes[1].price }</p>
+          </Col>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"></img>
+            <h4>{ shoes[2].title }</h4>
+            <p>{ shoes[2].price }</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
